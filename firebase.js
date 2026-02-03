571269f68a9acef5fe92c829a651c5f5cdfd68f4
gebe-ux <gbzz2010@gmail.com>
2026-02-02 21:56:53 -0300

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyA37lBu6vQm50Qhaq6YHX0wKSCsg1GqOI4",
+  authDomain: "skyblack-f0a9b.firebaseapp.com",
+  projectId: "skyblack-f0a9b",
+  storageBucket: "skyblack-f0a9b.appspot.com",
+  messagingSenderId: "941735366204",
+  appId: "1:941735366204:web:2269cf35f128421f63bd39"
+};
